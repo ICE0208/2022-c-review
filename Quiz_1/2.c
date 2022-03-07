7e9e2ecfb6717086fad42d0965b25c38d9fb83a0
ICE0208 <doky3307@naver.com>
2022-03-07 20:47:28 +0900

    2.c done

---
+++ Quiz_1/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("Then the jagged edge appears.\n");
+    printf("Through the distant clouds of tears\n");
+    printf("I like a bridge that has washed away.\n");
+    printf("My foundations were made of clay.");
+
+    return 0;
+}
